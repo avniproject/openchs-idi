07e127bc172a7fa781ab98ef5e5e8385cf37188b
sidtharthan <sidtharthann@samanvayfoundation.org>
2019-12-30 17:15:26 +0530

    #0 Deploy create_organisation.sql only when available

--- template.mk
+++ template.mk
@@ -5,7 +5,13 @@ su:=$(shell id -un)
 
 #{taskDefinitionsPerENV}
 
-create_org:; psql -U$(su) -d openchs < create_organisation.sql
+create_org:
+ifneq (,$(wildcard ./create_organisation.sql))
+	psql -U$(su) -d openchs < ./create_organisation.sql
+else
+	@echo
+endif
+
 create_views:; psql -U$(su) -d openchs < create_views.sql
 
 -include ./node_modules/openchs-idi/reports/reports.mk
